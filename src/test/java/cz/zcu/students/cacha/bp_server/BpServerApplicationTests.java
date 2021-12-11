16d8c30f7186b40eaed776c4037d2ffdfc4e56af
Pavel Čácha <pavelcacha@email.cz>
2021-12-11 15:02:18 +0100

    init

---
+++ src/test/java/cz/zcu/students/cacha/bp_server/BpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.zcu.students.cacha.bp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
