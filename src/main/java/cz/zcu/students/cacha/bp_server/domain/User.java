d7f9e64f95ffe33fae821a3034bc5a41a360a1ce
Pavel Čácha <pavelcacha@email.cz>
2022-05-02 11:23:15 +0200

    password max length removed

--- src/main/java/cz/zcu/students/cacha/bp_server/domain/User.java
+++ src/main/java/cz/zcu/students/cacha/bp_server/domain/User.java
@@ -47,7 +47,7 @@ public class User implements UserDetails {
      * hashed password
      */
     @NotNull(message = "Password can not be blank")
-    @Size(min = 8, max = 50, message = "Password must be between 8 to 50 letters long")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message="Password must contain a lowercase and an uppercase letter and a number")
     private String password;
 
